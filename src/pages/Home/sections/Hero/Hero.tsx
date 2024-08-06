e57df82da38fbd6066c67f637ca07b07571c916e
cassioestevao <cassioestevaops@gmail.com>
2024-08-06 11:05:35 -0300

    Docs: Reajuste no ambiente de trabalho

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,19 @@
+import { styled } from "@mui/material"
+
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(()=>({
+      backgroundColor: "black"
+    }))
+
+    return (
+      <>
+        <StyledHero>
+          Olá
+        </StyledHero>
+      </>
+    )
+  }
+  
+  export default Hero
