33c7ca4f42360ed8222659b5dbdac2ea8ef3896e
cassioestevao <cassioestevaops@gmail.com>
2024-08-06 11:38:45 -0300

    Docs: Definindo imagens e componentes

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,16 +1,21 @@
 import { styled } from "@mui/material"
-
+import Avatar from "../../../../assets/images/img.png"
 
 const Hero = () => {
 
     const StyledHero = styled("div")(()=>({
       backgroundColor: "black"
     }))
+    const StyledImg = styled("img")(()=>({
+      width: "30px"
+      
+    }))
 
     return (
       <>
         <StyledHero>
           Olá
+          <img src={Avatar} alt="" />
         </StyledHero>
       </>
     )
